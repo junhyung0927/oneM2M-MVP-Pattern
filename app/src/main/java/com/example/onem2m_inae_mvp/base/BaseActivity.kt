d2db6e418dd2f491ae1c596753bf332c57e34764
Junhyung <junhyung@edu.hanbat.ac.kr>
2021-08-25 18:40:56 +0900

    Base     - BaseActivity     - BasePresenter     - BaseView
    Resolves: None
    See also: None

---
+++ app/src/main/java/com/example/onem2m_inae_mvp/base/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.onem2m_inae_mvp.base
+
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatMultiAutoCompleteTextView
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+class BaseActivity: AppCompatActivity() {
+    protected fun <VB: ViewDataBinding> binding(@LayoutRes resId: Int): Lazy<VB> =
+        lazy { DataBindingUtil.setContentView<VB>(this, resId) }
+}
---
+++ app/src/main/java/com/example/onem2m_inae_mvp/base/BasePresenter.kt
@@ -0,0 +1,5 @@
+package com.example.onem2m_inae_mvp.base
+
+interface BasePresenter {
+    fun start()
+}
