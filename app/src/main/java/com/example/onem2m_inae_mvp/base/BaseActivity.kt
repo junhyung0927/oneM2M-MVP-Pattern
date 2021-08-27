ce3d2a10ec42363f1e318068a93116053bbb706a
Junhyung <junhyung@edu.hanbat.ac.kr>
2021-08-27 16:50:56 +0900

    ViewBinding     - build gradle ViewBinding 의존 추가     - BaseActivity binding 추가
    Resolves: None
    See also: Nones

--- app/src/main/java/com/example/onem2m_inae_mvp/base/BaseActivity.kt
+++ app/src/main/java/com/example/onem2m_inae_mvp/base/BaseActivity.kt
@@ -1,14 +1,26 @@
 package com.example.onem2m_inae_mvp.base
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
 
-abstract class BaseActivity: AppCompatActivity() {
+abstract class BaseActivity<VB: ViewBinding>: AppCompatActivity() {
     private lateinit var presenter: BasePresenter
+    protected lateinit var binding: VB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = inflateLayout(layoutInflater)
+    }
+
+    abstract fun inflateLayout(layoutInflater: LayoutInflater): VB
+
+    abstract val layoutId: Int
 
     override fun onDestroy() {
         super.onDestroy()
         presenter.onClear()
     }
-
-    abstract val layoutId: Int
 }
--- app/src/main/java/com/example/onem2m_inae_mvp/view/register/ContainerRegisterActivity.kt
+++ app/src/main/java/com/example/onem2m_inae_mvp/view/register/ContainerRegisterActivity.kt
@@ -2,12 +2,15 @@ package com.example.onem2m_inae_mvp.view.register
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.LayoutInflater
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContract
 import com.example.onem2m_in_ae.model.ContainerType
 import com.example.onem2m_inae_mvp.R
 import com.example.onem2m_inae_mvp.base.BaseActivity
+import com.example.onem2m_inae_mvp.databinding.ActivityContainerRegisterBinding
 
-class ContainerRegisterActivity() : BaseActivity(), ContainerRegisterContract.View {
+class ContainerRegisterActivity() : BaseActivity<ActivityContainerRegisterBinding>(), ContainerRegisterContract.View {
     companion object {
         private var pos: Int = -1
         private val containerType = listOf(
@@ -20,12 +23,17 @@ class ContainerRegisterActivity() : BaseActivity(), ContainerRegisterContract.Vi
     override val layoutId: Int
         get() = R.layout.activity_container_register
 
+    override fun inflateLayout(layoutInflater: LayoutInflater) =
+        ActivityContainerRegisterBinding.inflate(layoutInflater)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layoutId)
+
     }
 
     override fun addTextChange() {
 
     }
+
 }
